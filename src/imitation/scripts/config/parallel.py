5184e656c8620fed2f65d6e1b6071febd6c89bf0
Steven H. Wang <wang.steven.h@gmail.com>
2019-12-12 10:39:00 -0800

    v12

--- src/imitation/scripts/config/parallel.py
+++ src/imitation/scripts/config/parallel.py
@@ -119,7 +119,7 @@ def tune_mountain_car_try_0():
 @parallel_ex.named_config
 def cartpole_debug_ok():
   sacred_ex_name = "train_adversarial"
-  run_name = "tune_cartpole_gail_try_0_v11"
+  run_name = "tune_cartpole_gail_try_0_v12"
   n_seeds = 3
   search_space = {
     "config_updates": dict(
